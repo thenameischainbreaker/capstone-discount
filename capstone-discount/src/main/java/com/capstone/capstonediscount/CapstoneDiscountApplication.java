4f7673e2782675567fbf01a5fe4f92575114d57b
Jasper Javillo <jasperjavillo@gmail.com>
2023-07-31 11:16:53 -0400

    Created capstone-discount project

---
+++ capstone-discount/src/main/java/com/capstone/capstonediscount/CapstoneDiscountApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.capstonediscount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneDiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneDiscountApplication.class, args);
+	}
+
+}
---
+++ capstone-discount/src/test/java/com/capstone/capstonediscount/CapstoneDiscountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.capstonediscount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDiscountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
